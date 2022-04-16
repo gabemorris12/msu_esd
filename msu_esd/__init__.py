7284ed279ce7b64f17fe2dbe9cf88c7306a6b1b8
Gabe Morris <gabemorris1231@gmail.com>
2022-04-16 15:10:44 -0500

    added ssu to all

--- msu_esd/__init__.py
+++ msu_esd/__init__.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-from .functions import f_T, Re, f, hardy_cross, log_mean_temp_difference
+from .functions import f_T, Re, f, hardy_cross, log_mean_temp_difference, SSU
 from .pipes import Pipe
 from .ntu_effectiveness import cross_flow_unmixed, parallel_single_pass, shell_and_tube_one_shell_pass, \
     counter_single_pass
@@ -10,4 +10,4 @@ THIS_DIR = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(THIS_DIR)
 
 __all__ = ['f_T', 'Re', 'f', 'hardy_cross', 'Pipe', 'cross_flow_unmixed', 'log_mean_temp_difference',
-           'parallel_single_pass', 'shell_and_tube_one_shell_pass', 'counter_single_pass']
+           'parallel_single_pass', 'shell_and_tube_one_shell_pass', 'counter_single_pass', 'SSU']
